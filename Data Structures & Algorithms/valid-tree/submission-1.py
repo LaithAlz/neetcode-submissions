b2230a08f3832305f1e8b74189780dcf773fa5c3
Laith <55361207+LaithAlz@users.noreply.github.com>
2026-08-24 17:03:20 -0400

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                x = parent[x]
+                print(f"x went from {x} to {parent[x]}")
+            return x
+        
+        for a,b in edges:
+            print("going into leader a:", a)
+            leader_a = find(a)
+            print("going into leader b: ", b)
+            leader_b = find(b)
+
+            if leader_a == leader_b:
+                return False
+            parent[leader_a] = leader_b
+        
+        return len(edges) == n - 1
